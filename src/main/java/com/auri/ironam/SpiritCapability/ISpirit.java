e66c863383f5d69898fc326910890b8fbe977662
Auri <swiftshadow34@gmail.com>
2017-04-25 10:43:23 -0500

    Initial commit, basic functionality of mod is monstly working

---
+++ src/main/java/com/auri/ironam/SpiritCapability/ISpirit.java
@@ -0,0 +1,16 @@
+package com.auri.ironam.SpiritCapability;
+
+
+/**
+ * Created by 1800855 on 4/10/17.
+ */
+public interface ISpirit {
+    float getSpiritPoints();
+
+    void setSpiritPoints(float points);
+
+    void addSpiritPoints(float points);
+
+    void subSpiritPoints(float points);
+
+}
